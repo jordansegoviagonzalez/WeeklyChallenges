0b80166e9aa34cabb984299a7b785f1402f0ff4d
Jordan Segovia <segoviajordan91@gmail.com>
2024-12-12 19:14:59 -0800

    Made changes and pushed the project to my repos in git

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -28,62 +28,68 @@ namespace ChallengesWithTestsMark8
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            // public : Makes this method available to use anywhere in the program.
-    
-            // int : The method will return an integer value (the smaller number).
-    
-            // GetSmallestNumber : The method name. It tells you that the method finds the smallest number.
-    
-            // (int number1, int number2) : These are the inputs (two numbers) you provide when calling the method. 
-            // These are the numbers being compared to find the smallest.
-    
-            //  return : Sends the result (the smallest number) back to where the method was called. It ends the method.
-    
-            // Math Math:is the Class that contains mathematical functions constants.
+            //This method takes two integers as input (number1, and number2)
+            // and returns the smaller of the two.
+            if (number1 < number2)
+            // if :statement that allows to execute different code blocks based on a specific condition, it provides a way to make decisions within your program.
+            // Compares the two numbers and returns the smaller one
+            {
+                
+                return number1;
+                // if number 1 is smaller, return it, (keyword "return")
+                
+            }
+
+            return number2;
+            // else or otherwise, number 2 is smaller, return it 
             
-            // Min() (a method that returns the smaller of the two numbers).
-            
-            // Math.Min(number1, number2): This compares number1 and number2 and returns the smaller number.
-            
-            return Math.Min(number1, number2);  
-            
-            // It compares number1 and number2, then returns the smaller value.
-            // number1 and number2 are the arguments (inputs) giving to Math.Min, which are the two numbers being compared.
+
         }
         public long Multiply(long factor1, long factor2)
-           //This Method adds two integers values and returns the result.
-           // Multiply is the names method
+            // This Method multiplies two long integer values.
+           // public: access modifier: this means the method is accessible from anywhere within the program.
+           // long: return type: this part declares that the method is public and returns a long value.
+           // Multiply: this is the name of the method, indicating its purpose.
            // parameter name : factor1
            // parameter name : factor2
-        {
-            return fartor1 * factor2;
-            // return statement tells the method to stop executing and send the value stored in the result 
+        { // Methods Body.
+            return factor1 * factor2; // this line multiples the two inputs values, factor1, and factor2
+            // return statement tells the method to stop executing and send the value stored in the result
+            // keyword "return".
             
         }
-        // public : access modifier, this specifies the visibility of the method, means the method can be accessed from other classes.
-        // string : return type, this indicates that the method will return a value of type "string"
-        // GetaGreeting: methods name, its 
-     
-        //
 
         public string GetGreeting(string nameOfPerson)
-            // public : access modifier, this specifies the visibility of the method, means the meothod can be accessed from other classes.
-            // string : return type, this indicates that the method will rerurn a value of type "string"
-            // GetaGreetping: methods name, its convention in C# to use PascalCase for  methods names, 
+            //This Method returns a greeting message back to the user.
+            // public : access modifier, this specifies the visibility of the method, means the method can be accessed from other classes.
+            // string : return type, this indicates that the method will return a value of type "string"
+            // GetGreeting : methods name, its convention in C# to use PascalCase for  methods names
             // (string nameOfPerson): parameters, this is the input that the method will use to generate a greeting.
         {
-            return $"Hello {nameOfPerson}!";
-            // return: specifies the return value that the method should send back.     
+            // Methods Body
+            if (nameOfPerson == "")
+                // Checks if the name is empty
+                return "Hello";
+                // if the name is empty, returns a generic greetin g
+            
+            else
+            {
+                return $"Hello, {nameOfPerson}!";
+                // if the name is not empty, returns a personalized greeting
+            }
+            
+            
+            
         }
         public string GetHey()
-            // public : acces modifier, this also mean that only accessible within its own class
-            // string : return type, its purpuse is to indicate what the method wuill return.
-            // GetHey() : is the methods name inplementing PascalCase, its naming convention for methods.
+            // public : access modifier, this also mean that only accessible within its own class
+            // string : return type, its purpose is to indicate what the method will return.
+            // GetHey() : is the methods name implementing PascalCase, its naming convention for methods.
             
         {
             return "Hey!";
-            // ret1urn :return keyword signals that the method should stop executing and return the specified value.
-            // "Hey!" : will be sent back to the caller.
+            // return :return keyword signals that the method should stop executing and return the specified value.
+            // "Hey!"  will be sent back to the caller.
 ;
         }
     }
